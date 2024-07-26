e9db9ddb461b8c6232feeb954ab55afc43ebb251
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-07-26 19:16:56 +0530

    Replaced Django Development Server with Gunicorn on starting Docker Container
    entrypoint.sh script starts 3 gunicorn workers on starting docker container django

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 # Apply database migrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+# Use gunicorn to run the application
+exec gunicorn ePubColab.wsgi:wsgi_application --bind 0.0.0.0:8000 --workers 3
